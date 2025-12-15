e9665e35c7391aeb939cf97abe747fde53d61aa0
Starlyn Developer <starlynreyess49@gmail.com>
2025-12-15 11:14:19 -0400

    feat(Countries): agregar bindings para ICountryService y ICountryRepository en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Src\Domain\Interfaces\CountryServiceInterface;
 use Illuminate\Support\ServiceProvider;
+use App\Src\Application\Services\CountryService;
+use App\Src\Domain\Interfaces\ICountryRepository;
+use App\Src\Domain\Interfaces\ICountryService;
+use App\Src\Infrastructure\Repositories\CountryRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ICountryService::class, CountryService::class);
+        $this->app->bind(ICountryRepository::class, CountryRepository::class);
     }
 
     /**
