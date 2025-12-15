a0133d4d8f082988f7d3d392b4f53166eefc74a5
Starlyn Developer <starlynreyess49@gmail.com>
2025-12-15 10:29:31 -0400

    feat(Countries): cambio en el archivo deletecountry.js a objeto literal en vez de función

--- public/js/deletecountry.js
+++ public/js/deletecountry.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
-    $('.btn-delete').on('click', function() {
-        const countryId = $(this).attr('id');
+const DeleteCountry = {
+    init: function() {
+        $('.btn-delete').on('click', this.handleClick.bind(this));
+    },
+    handleClick: function(event) {
+        const countryId = $(event.currentTarget).attr('id');
         const deleteUrl = `/countries/${countryId}`;
-
         $('#delete-form').attr('action', deleteUrl);
         $('#delete-modal').modal('show');
-    });
+    }
+};
 
+$(document).ready(function() {
+    DeleteCountry.init();
 });
